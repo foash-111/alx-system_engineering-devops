76a7b1bab5834e3e91e4ed2514bae45a0623bfda
foash-111 <mr2221@fayoum.edu.eg>
2024-04-20 00:33:25 +0200

    configuration management, automation using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install flask
 package { 'flask':
 # You can specify 'present' or 'latest' to ensure it's installed or up-to-date
-ensure   => 'installed',
+ensure   => '2.1.0',
 provider => 'pip3' # Use pip3 as the package provider.
 }
