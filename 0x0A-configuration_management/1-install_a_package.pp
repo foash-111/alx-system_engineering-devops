c61f553f3d65a1f2cc2d2458e5ea20e3a0c5ac47
foash-111 <mr2221@fayoum.edu.eg>
2024-04-22 00:25:42 +0200

    Using Puppet, install flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-# Ensure the python3-pip package is installed
-package { 'python3-pip':
-  ensure => 'installed'
-}
-
+# Using Puppet, install flask from pip3.
 package { 'flask':
-  ensure => '2.1.0'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
