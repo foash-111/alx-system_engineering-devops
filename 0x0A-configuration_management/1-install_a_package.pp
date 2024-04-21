83763b837f902e7ba3768e537a352d82bddc1039
foash-111 <mr2221@fayoum.edu.eg>
2024-04-21 17:14:59 +0200

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,4 @@
-# install flask
+# Ensure the python3-pip package is installed
 package { 'flask':
-# You can specify 'present' or 'latest' to ensure it's installed or up-to-date
-ensure   => '2.1.0',
-provider => 'pip3' # Use pip3 as the package provider.
+  ensure => '2.1.0'
 }
