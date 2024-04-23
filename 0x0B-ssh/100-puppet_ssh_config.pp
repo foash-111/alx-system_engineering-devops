5eb658ca6cd523d553a40d33a781494e2455b342
foash-111 <mr2221@fayoum.edu.eg>
2024-04-23 17:57:45 +0200

    puppet configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,5 +3,5 @@
 file { 'config':
   ensure  => 'present',
   path    => ' /etc/ssh/ssh_config',
-  content => 'Host *\n\t IdentityFile ~/.ssh/school\n\t PasswordAuthentication no\n'
+  content => 'Host *\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no\n'
 }
