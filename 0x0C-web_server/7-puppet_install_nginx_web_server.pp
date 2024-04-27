b28f276a0ca8474b41bd96f3fcafeb9b011e68fc
foash-111 <mr2221@fayoum.edu.eg>
2024-04-27 16:12:19 +0300

    mainfest to install nginx and it's files in the server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,43 @@
-# Puppet manifest containing commands to automatically configure an Ubuntu machine 
+# Puppet manifest containing commands to automatically configure an Ubuntu machine
 
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
+# Create index.html with "Hello World!" content
 file {'/var/www/html/index.html':
   ensure  => file,
-  content => '<h1>Hello World!</h1>'
+  content => '<h1>Hello World!</h1>',
 }
 
+# Define Nginx server configuration
 file { '/etc/nginx/sites-available/default':
-  ensure  => file
+  ensure  => file,
   content => "
-  server {
-    listen 80 default_server;
-    listen [::]:80 default server;
-    root /var/www/html;
-    index index.html;
-    server_name foash.tech;
-    location /redirect_me {
-      return 301 https://www.youtube.com/;
-    }
-    "
-}
+server {
+  listen 80 default_server;
+  listen [::]:80 default server;
+  root /var/www/html;
+  index index.html;
+  server_name foash.tech;
 
-file { '/etc/nginx/sites-enabled/default'
-  ensure  => link,
-  require => FILE['/etc/nginx/sites-available/default']
-  target  => '/etc/nginx/sites-available/default'
-
+  location /redirect_me {
+    return 301 https://www.youtube.com/;
   }
 
+}
+",
+}
+
+# Ensure the symbolic link is created for Nginx configuration
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Restart Nginx service after configuration changes
 service { 'nginx':
   ensure    => running,
   enable    => true,
